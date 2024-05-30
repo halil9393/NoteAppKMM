9e33b85916e944243320dd266869ac1e1b1eafc7
hik <halilibrahimk.93@gmail.com>
2024-05-30 11:46:52 +0300

    First commit- gradle setup finished

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    //trick: for the same plugin versions in all sub-modules
+    alias(libs.plugins.androidApplication).apply(false)
+    alias(libs.plugins.androidLibrary).apply(false)
+    alias(libs.plugins.kotlinAndroid).apply(false)
+    alias(libs.plugins.kotlinMultiplatform).apply(false)
+    id("com.squareup.sqldelight").version("1.5.3").apply(false)
+    id("com.google.dagger.hilt.android").version("2.42").apply(false)
+}
---
+++ shared/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    id("com.squareup.sqldelight")
+}
+
+kotlin {
+    androidTarget {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
+    
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "shared"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            //put your multiplatform dependencies here
+            implementation("com.squareup.sqldelight:runtime:1.5.3")
+            implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
+        }
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+        }
+        androidMain.dependencies {
+            implementation("com.squareup.sqldelight:android-driver:1.5.3")
+        }
+        iosMain.dependencies {
+            implementation("com.squareup.sqldelight:native-driver:1.5.3")
+        }
+    }
+}
+
+android {
+    namespace = "com.example.noteappkmm"
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 24
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
